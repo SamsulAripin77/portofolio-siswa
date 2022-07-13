7740a752b7580efc9d2299b4df0a6031f2618d19
SamsulAripin77 <arifins192@gmail.com>
2022-07-13 22:50:03 +0700

    fix (skill dropdown)

--- resources/views/components/skill-create.blade.php
+++ resources/views/components/skill-create.blade.php
@@ -1,1 +1,9 @@
-<x-input field="skill" lable="Skill"/>
+<div class="col-span-6 sm:col-span-4 my-4">
+<select name="skill" id="" class="mt-1 block w-full shadow-md">
+    <option value="Programming">Programming</option>
+    <option value="Copy writing">Copy Writing</option>
+    <option value="Design">Design</option>
+    <option value="Public Speaking">Public Speaking</option>
+    <option value="Photography">Photography</option>
+</select>
+</div>
